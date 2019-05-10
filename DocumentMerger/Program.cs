d533ee01d3a4f90c23e657a8d5c4f8e4a21d19c9
Matt Bonderer <mcbonderer10@gmail.com>
2019-05-09 22:32:55 -0500

    Initial commit of .vs project files

---
+++ DocumentMerger/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            shuck my balls matt
+        }
+    }
+}
